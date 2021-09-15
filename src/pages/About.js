fb3ea6a8dec54ee877631f6bb0a7fdc93ec5de7d
Almog Wertzberger <Almogish@gmail.com>
2021-09-15 14:30:08 +0300

    adding loading/about/error pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis sed
+        fugiat voluptate tenetur tempore, dolorum vitae? Nisi eum consequatur
+        vero facilis deserunt voluptates eaque doloribus repellendus eius
+        maiores numquam, dolorum praesentium doloremque iste suscipit, omnis et.
+        Odio porro aut ipsa?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
